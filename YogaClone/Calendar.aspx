215250cf7068bf6352db6e7c654e884f3e4005f9
Jasmine Logan <jasmine.logan@pharmacyfirst.com>
2020-06-29 23:28:07 -0500

    Class calendar added

---
+++ YogaClone/Calendar.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page  Title="Calendar" MasterPageFile="~/Site.Master" Language="C#" AutoEventWireup="true" CodeBehind="Calendar.aspx.cs" Inherits="YogaClone.Calendar" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <link href="Content/Calendar.css" rel="stylesheet" />
+     <div  class="defaultPage">
+        <div class="pgContainer " >
+            <div class="slogan-container" id="pic-padding">
+                 <h2 class="Calendar">Calendar</h2>
+
+                <div class="DateContainer" style="margin-left:30px;">
+
+                    <asp:Calendar runat="server" BackColor="White" BorderColor="White" BorderWidth="1px" Font-Names="Verdana" Font-Size="9pt" ForeColor="Black" Height="333px" NextPrevFormat="FullMonth" Width="558px" ID="Calendar1" OnSelectionChanged="Calendar1_SelectionChanged" FirstDayOfWeek="Sunday" OnDayRender="Calendar1_DayRender">
+                        <DayHeaderStyle Font-Bold="True" Font-Size="8pt" />
+                        <NextPrevStyle Font-Bold="True" Font-Size="8pt" ForeColor="#333333" VerticalAlign="Bottom" />
+                        <OtherMonthDayStyle ForeColor="#999999" />
+                        <SelectedDayStyle BackColor="#333399" ForeColor="White" />
+                        <TitleStyle BackColor="White" BorderColor="Black" BorderWidth="4px" Font-Bold="True" Font-Size="12pt" ForeColor="#333399" />
+                        <TodayDayStyle BackColor="#CCCCCC" />
+                        <WeekendDayStyle BackColor="#339966" />
+                    </asp:Calendar>
+               
+
+                    <br />
+                    <asp:DropDownList ID="LstTimes" runat="server" >
+                    </asp:DropDownList>
+                    <br />
+                    <br />
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+                    <br />
+                    <br />
+                    <asp:Label ID="lblAvailClasses" runat="server" Text=""></asp:Label>
+               
+                 
+               
+
+                </div>
+            </div>
+        </div>
+        <asp:Image ID="yogaLady" runat="server" ImageUrl="images/lady-in-yoga.jpg" CssClass="yogaLady" />
+    </div>
+</asp:Content>
---
+++ YogaClone/Calendar.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace YogaClone
+{
+    public partial class Calendar : System.Web.UI.Page
+    {
+        //www.codeproject.com/Articles/378900/Calendar-NET
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblAvailClasses.Text = "We look forward to seeing you  " + Calendar1.SelectedDate.ToLongDateString() + "<br/>" + LstTimes.SelectedValue;
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.IsWeekend)
+            {
+                e.Day.IsSelectable = false;
+            }
+            if(e.Day.Date.Day== 4 && e.Day.Date.Month == 7)
+            {
+                e.Cell.BackColor = System.Drawing.Color.LightGreen;
+                Label Lbl = new Label();
+                Lbl.Text = "<br/> 4th of July";
+                e.Cell.Controls.Add(Lbl);
+                e.Day.IsSelectable = false;
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            //lblAvailClasses.Text = "Classes <br/>";
+            //lblAvailClasses.Text = Calendar1.SelectedDate.ToLongDateString();
+            LstTimes.Items.Clear();
+
+            switch (Calendar1.SelectedDate.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    LstTimes.Items.Add("8:00AM-9AM : Beginners Yoga");
+                    LstTimes.Items.Add("11:00AM-12:00PM : Intermediate Yoga");
+                    LstTimes.Items.Add("1:00AM-12:00PM : Advanced Yoga");
+                    break;
+                case DayOfWeek.Wednesday:
+                    LstTimes.Items.Add("8:00AM-9AM : Beginners Yoga");
+                    LstTimes.Items.Add("11:00AM-12:00PM : Intermediate Yoga");
+                    LstTimes.Items.Add("1:00AM-12:00PM : Advanced Yoga");
+                    break;
+                case DayOfWeek.Friday:
+                    LstTimes.Items.Add("8:00AM-9AM : Beginners Yoga");
+                    LstTimes.Items.Add("11:00AM-12:00PM : Intermediate Yoga");
+                    LstTimes.Items.Add("1:00AM-12:00PM : Advanced Yoga");
+                    break;
+                default:
+                    LstTimes.Items.Add("10:00AM-11AM : Beginners Yoga");
+                    LstTimes.Items.Add("12:00PM-1:00PM : Intermediate Yoga");
+                    LstTimes.Items.Add("2:00PM-3:00PM : Advanced Yoga");
+                    break;
+
+            }
+
+        }
+
+    
+    }
+}
